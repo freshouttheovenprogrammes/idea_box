f7997afc3256cf2dcd62e4144ed238746e6f26ab
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-10 12:30:01 -0700

    fix rails_helper to work with should-matcher / use proper should validate

--- spec/models/idea/idea_spec.rb
+++ spec/models/idea/idea_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 describe Idea, type: :model do
-      it {should validate_presence_of(:name, :user_id, :category_id)}
+  it { should validate_presence_of(:name)}
+  it { should validate_presence_of(:user_id)}
+  it { should validate_presence_of(:category_id)}
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,6 +58,7 @@ end
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
+    with.test_framework :rspec
     with.library :rails
   end
 end
