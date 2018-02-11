ac4e36055aa973b9cb03e7eb5e9b5d5a05caa2e5
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-11 11:00:17 -0700

    password more secure with bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :email, :password, :full_name
   validates :email, uniqueness: { case_sensitive: false, message: "Sorry there is a user already registered with that email, please try again" }
   has_many :ideas
+  has_secure_password
   def self.authenticate(email, password)
   end
 
