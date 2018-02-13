ae287e7df85e9d04030a01f8c215896ec3b782a4
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-13 07:42:35 -0700

    added a bunch of carrierwave requires and fixed un init constant for uploader

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
-describe "Image Model" do
-  pending("something else getting finished") do
-    raise "Item broken because of uninitialized constant Image::ImageUploader" do
-    it {should validate_presence_of(:title)}
-    end
-  end
+describe Image, type: :model do
+  it {should validate_presence_of(:title)}
 end
