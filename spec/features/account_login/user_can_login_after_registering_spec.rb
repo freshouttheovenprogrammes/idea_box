98e84c06485613c5cf5b373e64fda5c3dc03a372
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-14 09:53:39 -0700

    adds edgecase for incorrect password

--- spec/features/account_login/user_can_login_after_registering_spec.rb
+++ spec/features/account_login/user_can_login_after_registering_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 describe "As a user" do
   context "after registering with IdeaBox" do
-    it "I can log back in" do
+    it "I can log back in and arrive at my profile" do
       user = create(:user)
-      # user = User.create!(email: "jon@gmail.com", password: "password", full_name: "Jon Jon", username: "jj101")
-      byebug
+
       visit root_path
 
       click_link 'Log In'
 
       expect(current_path).to eq login_path
 
-      fill_in 'Email', with: 'jon@gmail.com'
+      fill_in 'Email', with: 'zac@zac.com'
       fill_in 'Password', with: 'password'
 
       click_on 'Log In'
@@ -20,6 +19,22 @@ describe "As a user" do
       expect(current_path).to eq user_path(user)
 
       expect(page).to have_content("Welcome back #{user.full_name}!")
+    end
+
+    it "if I enter the wrong password I am directed back to the login page" do
+      user = create(:user)
+      visit root_path
+
+      click_link 'Log In'
+
+      expect(current_path).to eq login_path
+
+      fill_in 'Email', with: 'zac@zac.com'
+      fill_in 'Password', with: 'wrongpassword'
+
+      click_on 'Log In'
+
+      expect(current_path).to eq login_path
     end
   end
 end
