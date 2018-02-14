2366eeeae29a49a852a5be0ce111c3545bf4c268
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-14 13:14:35 -0700

    fix user view their own idea test

--- spec/features/idea/user_can_view_their_idea_spec.rb
+++ spec/features/idea/user_can_view_their_idea_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 describe "As a user" do
+  before(:each) do
+    @user = create(:user)
+    @category = create(:category)
+    visit root_path
+
+    click_on 'Log In'
+
+    fill_in 'Email', with: "zac@zac.com"
+    fill_in 'Password', with: "password"
+
+    click_on 'Log In'
+  end
   context "after I create an idea" do
     it "I can view the details of that idea" do
-      user = create(:user)
-      category = create(:category)
-      idea = Idea.create!(name: "take out trash", user_id: user.id, category_id: category.id)
-      visit user_idea_path(user, idea)
+      idea = Idea.create!(name: "take out trash", user_id: @user.id, category_id: @category.id)
+      visit user_idea_path(@user, idea)
 
       expect(page).to have_content("take out trash")
     end
