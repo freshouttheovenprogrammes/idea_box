173ddd59c1b0bac1f22346b9069e4c9a81985758
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-14 13:12:30 -0700

    fix user can see categories or category test

--- spec/features/category/user_can_see_specific_category_spec.rb
+++ spec/features/category/user_can_see_specific_category_spec.rb
@@ -1,25 +1,34 @@
 require 'rails_helper'
 
 describe "As a user" do
+  before(:each) do
+    @user = create(:user)
+    @category = create(:category)
+    visit root_path
+
+    click_on 'Log In'
+
+    fill_in 'Email', with: "zac@zac.com"
+    fill_in 'Password', with: "password"
+
+    click_on 'Log In'
+  end
   context "when I go to the category index" do
     it "I see all the categories" do
-      category = Category.create!(name: "chores")
-      category2 = Category.create!(name: "smores")
+      category2 = create(:category, name: "smores", id: 2)
 
       visit categories_path
 
-      expect(page).to have_content category.name
+      expect(page).to have_content @category.name
       expect(page).to have_content category2.name
     end
     it "I click on the category name and go to the category show page" do
-      category = Category.create!(name: "chores")
-
       visit categories_path
 
-      click_link(category.name)
+      click_link(@category.name)
 
-      expect(current_path).to eq category_path(category)
-      expect(page).to have_content(category.name)
+      expect(current_path).to eq category_path(@category)
+      expect(page).to have_content(@category.name)
     end
   end
 end
