339401c9fbeb69d325bd4d04aa559c42c1aae66c
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-15 19:16:45 -0700

    adds seeds to play with for website

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Image.destroy_all
+Category.destroy_all
+
 User.create!(email: "boss@gmail.com", password: "bossperson", full_name: "CEO", role: 1)
+
+Image.create!(title:"Dawg", image: 'app/assets/images/IMG_2433.jpg' )
+Image.create!(title:"Rambo", image: 'app/assets/images/IMG_2445.jpg' )
+Image.create!(title:"Peaceful", image: 'app/assets/images/IMG_2711.jpg' )
+Image.create!(title:"Meow", image: 'app/assets/images/IMG_2974.jpg' )
+Image.create!(title:"One Cool Dude", image: 'app/assets/images/IMG_3282.jpg' )
+Image.create!(title:"Bork", image: 'app/assets/images/IMG_3315.jpg' )
+
+5.times do |n|
+  Category.create!(name: "Category #{n}")
+end
