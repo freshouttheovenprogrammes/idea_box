f61f0d643af8ddc69a5a9e79b60999974b2c36bb
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-15 19:29:50 -0700

    add default images to asset pipeline

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@ Category.destroy_all
 
 User.create!(email: "boss@gmail.com", password: "bossperson", full_name: "CEO", role: 1)
 
-Image.create!(title:"Dawg", image: 'app/assets/images/IMG_2433.jpg' )
-Image.create!(title:"Rambo", image: 'app/assets/images/IMG_2445.jpg' )
-Image.create!(title:"Peaceful", image: 'app/assets/images/IMG_2711.jpg' )
-Image.create!(title:"Meow", image: 'app/assets/images/IMG_2974.jpg' )
-Image.create!(title:"One Cool Dude", image: 'app/assets/images/IMG_3282.jpg' )
-Image.create!(title:"Bork", image: 'app/assets/images/IMG_3315.jpg' )
+Image.create!(title:"Dawg", image: 'IMG_2433.jpg' )
+Image.create!(title:"Rambo", image: 'IMG_2445.jpg' )
+Image.create!(title:"Peaceful", image: 'IMG_2711.jpg' )
+Image.create!(title:"Meow", image: 'IMG_2974.jpg' )
+Image.create!(title:"One Cool Dude", image: 'IMG_3282.jpg' )
+Image.create!(title:"Bork", image: 'IMG_3315.jpg' )
 
 5.times do |n|
   Category.create!(name: "Category #{n}")
