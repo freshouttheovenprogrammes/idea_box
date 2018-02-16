dfc9df8e4d5081987e46a3750367667f3a5d58f0
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-16 07:21:31 -0700

    temporary patch for image_tag issue I'm having

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def image_tag(img , options={})
+    path = "#{Rails.root}/app/assets/images/#{img}"
+    img = "missing.png"  unless img.present? and File.file?(path)
+    super(img, options)
+  end
 end
