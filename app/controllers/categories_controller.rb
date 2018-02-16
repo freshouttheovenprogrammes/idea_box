7e236a631cec3a94f052d414e4e356e83e09a6e0
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-16 05:39:19 -0700

    fix edit category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,6 +28,21 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Success"
+      redirect_to category_path(@category)
+    else
+      flash[:notice] = "Try again"
+      render :edit
+    end
+  end
+
   private
 
  def category_params
