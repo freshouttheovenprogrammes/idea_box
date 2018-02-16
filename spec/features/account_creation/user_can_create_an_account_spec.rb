2238bb92d2457d3592bc30f8e3407a010b7f01f9
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-16 05:13:29 -0700

    adjust test to reflect changes

--- spec/features/account_creation/user_can_create_an_account_spec.rb
+++ spec/features/account_creation/user_can_create_an_account_spec.rb
@@ -14,17 +14,6 @@ describe "As a user" do
       expect(page).to have_selector(:link_or_button, 'Log In')
     end
 
-    it "I enter my credentials and arrive at new user page" do
-      visit root_path
-
-      fill_in 'Email Address', with: "example@email.com"
-      fill_in 'Password', with: "password"
-      fill_in 'Full Name', with: "Joe Smith"
-      click_on 'Get Started'
-
-      expect(current_path).to eq new_user_path
-    end
-
     it "without entering password I get an error message" do
       visit root_path
 
