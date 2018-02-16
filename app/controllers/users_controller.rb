3666eb1d7592c6e2dcd6c03c469c8d42d51f93d8
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-15 18:37:55 -0700

    fix root page enrollment and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :full_name)
   end
 end
